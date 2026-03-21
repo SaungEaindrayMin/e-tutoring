3fe8f162d99b8b877798f2562e6fac3944aa8f05
james135422-cmd <james135422@gmail.com>
2026-03-21 18:05:40 +0700

    fix confilct

--- src/RouteComponent.jsx
+++ src/RouteComponent.jsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Login from "./views/login/Login";
 import ForgetPassword from "./views/login/ForgetPassword";
 import OtpVerify from "./views/login/OtpVerify";
@@ -13,9 +14,77 @@ import Message from "./views/message/Message";
 import BlogList from "./views/blog/BlogList";
 import BlogDetail from "./views/blog/BlogDetail";
 import Allocate from "./views/allocate/Allocate";
+import Analytics from "./views/analytics/Analytics";
+import DataServices from "./services/data-services";
+import Configuration from "./services/configuration";
 
+const dataService = new DataServices();
+const config = new Configuration();
+
+const EXCLUDED_ACTIVITY_PATHS = new Set([
+  "/",
+  "/login",
+  "/forget-password",
+  "/email-otp-verify",
+  "/verify-success",
+]);
+
+const PAGE_NAME_BY_PATH = {
+  "/": "Login",
+  "/login": "Login",
+  "/forget-password": "Forget Password",
+  "/email-otp-verify": "OTP Verify",
+  "/verify-success": "Verify",
+  "/change-password": "Change Password",
+  "/admin": "Dashboard",
+  "/admin/meetings": "Meetings",
+  "/admin/users": "Create Account",
+  "/admin/messages": "Messages",
+  "/admin/blog": "Blog",
+  "/admin/allocate-tutor": "Allocate Tutor",
+  "/admin/visit-analytics": "Visit Analytics",
+  "/admin/documents": "Documents",
+};
+
+const toTitleCase = (value) =>
+  value
+    .replace(/-/g, " ")
+    .replace(/\b\w/g, (char) => char.toUpperCase());
+
+const getPageNameFromPath = (pathname) => {
+  if (pathname.startsWith("/admin/blog/")) {
+    return "Blog";
+  }
+
+  if (PAGE_NAME_BY_PATH[pathname]) {
+    return PAGE_NAME_BY_PATH[pathname];
+  }
+
+  const segments = pathname.split("/").filter(Boolean);
+
+  if (!segments.length) {
+    return "Home";
+  }
+
+  return toTitleCase(segments[segments.length - 1]);
+};
 
 const RouteComponent = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (EXCLUDED_ACTIVITY_PATHS.has(location.pathname)) {
+      return;
+    }
+
+    const pageName = getPageNameFromPath(location.pathname);
+    const serviceName = config.SERVICE_NAME + config.SERVICE_USER_ACTIVITY;
+
+    dataService.retrievePOST({ page: pageName }, serviceName).catch((error) => {
+      console.error("Failed to track user activity:", error);
+    });
+  }, [location.pathname]);
+
   return (
     <Routes>
       <Route path="/" element={<Login />} />
