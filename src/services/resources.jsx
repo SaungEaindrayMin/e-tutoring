8abf59e962bd1d8d045f45fdbb489045c495afef
james135422-cmd <james135422@gmail.com>
2026-04-09 21:48:49 +0700

    added pro socket url

--- src/services/resources.jsx
+++ src/services/resources.jsx
@@ -1,12 +1,12 @@
 class Resources {
   APP_NAME = "";
-  BACKEND_SIDE_BASE_URL = "https://kmd.twistserver.online/api";
+  // BACKEND_SIDE_BASE_URL = "https://kmd.twistserver.online/api";
   // BACKEND_SIDE_BASE_URL = "http://localhost:3001/api";
   // BACKEND_SIDE_SOCKET_URL = "http://localhost:3001";
   // BACKEND_SIDE_BASE_URL = "https://dev-kmd-cms-api.onrender.com/api";
-  BACKEND_SIDE_SOCKET_URL = "https://dev-kmd-cms-api.onrender.com";
-  // BACKEND_SIDE_BASE_URL = "https://kmd.twistserver.online/api";
-  // BACKEND_SIDE_SOCKET_URL = "https://kmd.twistserver.online";
+  // BACKEND_SIDE_SOCKET_URL = "https://dev-kmd-cms-api.onrender.com";
+  BACKEND_SIDE_BASE_URL = "https://kmd.twistserver.online/api";
+  BACKEND_SIDE_SOCKET_URL = "https://kmd.twistserver.online";
 }
 
 export default Resources;
